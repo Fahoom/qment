8be667db617b444dadbd9579f7b4907491458fb4
Fahoom <justfahoom@gmail.com>
2022-12-10 13:30:20 +0000

    made the tab area look a bit nicer, and cargo fmt

--- src/app/editor.rs
+++ src/app/editor.rs
@@ -1,7 +1,10 @@
 use std::path::PathBuf;
 
 use crate::document::Document;
-use eframe::egui::{CentralPanel, Key, SidePanel, TextEdit, TopBottomPanel, Ui, ScrollArea};
+use eframe::egui::{
+    Button, CentralPanel, Frame, Key, ScrollArea, Separator, SidePanel, TextEdit, TopBottomPanel,
+    Ui,
+};
 
 pub struct Editor {
     pub document: Document,
@@ -32,74 +35,88 @@ impl Editor {
         let Some(num) = self.current_question else { return; };
         let Some(question) = self.document.get_question_mut(num) else { return; };
 
-        TopBottomPanel::top("TagPanel").show_inside(ui, |ui| {
-            ui.horizontal(|ui| {
-                ui.label("Tags");
-                if ui.button("+").clicked() {
-                    self.ghost_state = GhostState::TagGroup(String::new(), None);
+        TopBottomPanel::top("TagPanel")
+            .frame(Frame::default())
+            .show_inside(ui, |ui| {
+                ui.horizontal(|ui| {
+                    ui.heading("Tags");
+                    if ui.add(Button::new("+").frame(false)).clicked() {
+                        self.ghost_state = GhostState::TagGroup(String::new(), None);
+                    }
+                });
+
+                ui.add_space(5.0);
+
+                let mut remove_group = None;
+
+                for (name, tags) in question.groups_mut() {
+                    ui.horizontal_wrapped(|ui| {
+                        let label = ui.add(Button::new(name).frame(false));
+                        ui.add(Separator::default().vertical());
+
+                        let mut remove_tag = None;
+                        for tag in tags.iter() {
+                            if ui.add(Button::new(tag).frame(false)).clicked() {
+                                remove_tag = Some(tag.to_owned());
+                            }
+                        }
+
+                        if let Some(remove_tag_name) = remove_tag {
+                            tags.remove(&remove_tag_name);
+                        }
+
+                        if ui.button("+").clicked() {
+                            self.ghost_state = GhostState::Tag(String::new());
+                        }
+
+                        if let GhostState::Tag(text) = &mut self.ghost_state {
+                            let resp = ui.add(TextEdit::singleline(text));
+                            if resp.lost_focus() || ui.input().key_pressed(Key::Escape) {
+                                if !text.is_empty() {
+                                    tags.insert(text.to_owned());
+                                }
+                                self.ghost_state = GhostState::Empty;
+                            }
+                            resp.request_focus();
+                        }
+
+                        label.context_menu(|ui| {
+                            if ui.button("Delete").clicked() {
+                                // Cant reborrow question mutably, so setup vars to remove after mut borrow
+                                remove_group = Some(name.clone());
+                            }
+                            if ui.button("Rename").clicked() {
+                                self.ghost_state =
+                                    GhostState::TagGroup(name.clone(), Some(name.clone()));
+                            }
+                        });
+                    });
+                }
+
+                if let Some(removed_group_name) = &remove_group {
+                    question.remove_group(removed_group_name);
+                }
+
+                if let GhostState::TagGroup(text, original) = &mut self.ghost_state {
+                    let resp = ui.add(TextEdit::singleline(text));
+
+                    if resp.lost_focus() || ui.input().key_pressed(Key::Escape) {
+                        if !text.is_empty() {
+                            if let Some(old_name) = original {
+                                question.rename_group(old_name, &text);
+                            } else if !question.has_group(&text) {
+                                question.add_group(&text);
+                            }
+                        }
+                        self.ghost_state = GhostState::Empty;
+                    }
+
+                    // Request after, otherwise we will never lose focus!
+                    resp.request_focus();
                 }
             });
 
-            let mut remove_group = None;
-
-            for (name, tags) in question.groups_mut() {
-                ui.horizontal_wrapped(|ui| {
-                    let label = ui.button(name);
-                    
-                    for tag in tags.iter() {
-                        if ui.button(tag).clicked() {}
-                    }
-
-                    if ui.button("+").clicked() {
-                        self.ghost_state = GhostState::Tag(String::new());
-                    }
-
-                    if let GhostState::Tag(text) = &mut self.ghost_state {
-                        let resp = ui.add(TextEdit::singleline(text));
-                        if resp.lost_focus() || ui.input().key_pressed(Key::Escape) {
-                            if !text.is_empty() {
-                                tags.insert(text.to_owned());
-                            }
-                            self.ghost_state = GhostState::Empty;
-                        }
-                        resp.request_focus();
-                    }
-                    
-                    label.context_menu(|ui| {
-                        if ui.button("Delete").clicked() {
-                            // Cant reborrow question mutably, so setup vars to remove after mut borrow
-                            remove_group = Some(name.clone());
-                        }
-                        if ui.button("Rename").clicked() {
-                            self.ghost_state = GhostState::TagGroup(name.clone(), Some(name.clone()));
-                        }
-                    });
-                    
-                });
-            }
-
-            if let Some(removed_group_name) = &remove_group {
-                question.remove_group(removed_group_name);
-            }
-
-            if let GhostState::TagGroup(text, original) = &mut self.ghost_state {
-                let resp = ui.add(TextEdit::singleline(text));
-
-                if resp.lost_focus() || ui.input().key_pressed(Key::Escape) {
-                    if !text.is_empty() {
-                        if let Some(old_name) = original {
-                            question.rename_group(old_name, &text);
-                        } else if !question.has_group(&text) {
-                            question.add_group(&text);
-                        } 
-                    }
-                    self.ghost_state = GhostState::Empty;
-                }
-
-                // Request after, otherwise we will never lose focus!
-                resp.request_focus();
-            }
-        });
+        ui.add(Separator::default().horizontal().spacing(20.0));
 
         ui.horizontal(|ui| {
             for (name, _) in question.sections() {
@@ -109,21 +126,23 @@ impl Editor {
             }
         });
 
-        let Some(section_name) = &self.current_section else { return; };
-        let Some(section) = question.get_section_mut(section_name) else { return; };
+        CentralPanel::default().show_inside(ui, |ui| {
+            let Some(section_name) = &self.current_section else { return; };
+            let Some(section) = question.get_section_mut(section_name) else { return; };
 
-        if let Some(text) = &mut section.text {
-            ui.add(TextEdit::multiline(text));
-        } else {
-            if ui.button("Add Text").clicked() {
-                section.text = Some(String::new());
+            if let Some(text) = &mut section.text {
+                ui.add(TextEdit::multiline(text).desired_width(ui.available_width()));
+            } else {
+                if ui.button("Add Text").clicked() {
+                    section.text = Some(String::new());
+                }
             }
-        }
+        });
     }
 
     pub fn draw_sidebar(&mut self, ui: &mut Ui) {
         TopBottomPanel::top("SidebarHeader").show_inside(ui, |ui| {
-            let highest = self.document.questions().fold(u32::MIN, |a,b| a.max(*b.0));
+            let highest = self.document.questions().fold(u32::MIN, |a, b| a.max(*b.0));
 
             ui.horizontal(|ui| {
                 ui.label("Questions");
@@ -142,19 +161,19 @@ impl Editor {
                             self.current_question = Some(*num)
                         }
                     }
-    
+
                     // Ghost Rendering
                     if let GhostState::Sidebar(text) = &mut self.ghost_state {
                         let resp = ui.add(TextEdit::singleline(text));
-    
+
                         if resp.lost_focus() || ui.input().key_pressed(Key::Escape) {
                             if let Ok(num) = text.parse::<u32>() {
                                 self.document.add_question(num);
                             }
-    
+
                             self.ghost_state = GhostState::Empty;
                         }
-    
+
                         // Request after, otherwise we will never lose focus!
                         resp.request_focus();
                     }
